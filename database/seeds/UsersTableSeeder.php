b9becf3267187a22e834ac831d7890636b8f1a1e
Alleo Indong <freedom_war550@live.com.ph>
2017-03-07 10:48:04 +0800

    added new user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\User::create([
+            'name'  => 'Admin Admin',
+            'email' => 'admin@wyeth.com',
+            'password' => bcrypt('password')
+        ]);
     }
 }
