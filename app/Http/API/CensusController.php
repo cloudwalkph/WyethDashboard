0f0ccf44cd11776f1147ea3ed104b22058f27f7e
Alleo Indong <freedom_war550@live.com.ph>
2017-03-07 11:59:46 +0800

    updated census

--- app/Http/API/CensusController.php
+++ app/Http/API/CensusController.php
@@ -10,8 +10,18 @@ class CensusController extends Controller {
     {
         $input = $request->all();
 
+        $duplicate = Census::where('name', $input['name'])
+            ->where('email', $input['email'])
+            ->first();
+
+        // Check for duplicate
+        if ($duplicate) {
+            return response()->json('duplicate', 400);
+        }
+
         $census = Census::create($input);
 
+        // Failed to create data
         if (! $census) {
             return response()->json('failed', 400);
         }
