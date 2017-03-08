dd39b39e35019cb9141112ab70ec8cb8e55ca0ce
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-08 13:13:45 +0800

    update

--- app/Census.php
+++ app/Census.php
@@ -10,4 +10,17 @@ class Census extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function parseBooleanZipIt()
+    {
+        return $this->attributes['promo_zip_it'] ? 'TRUE' : 'FALSE';
+    }
+    public function parseBooleanCrayola()
+    {
+        return $this->attributes['promo_crayola_bunding'] ? 'TRUE' : 'FALSE';
+    }
+    public function parseBooleanNone()
+    {
+        return $this->attributes['promo_none'] ? 'TRUE' : 'FALSE';
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,36 +4,41 @@
     <div class="row">
         <div class="col-md-12">
 
-            <h2> Wyeth Promil Dashboard </h2>
-
-            <table class="table table-hover" id="myTable">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Mobile</th>
-                    <th>Email</th>
-                    <th>Promo Crayola Bunding</th>
-                    <th>Promo Zip It</th>
-                    <th>Promo None</th>
-                    <th>Date Inputted</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($census as $c)
-                    <tr>
-                        <td>{{ $c->name }}</td>
-                        <td>{{ $c->age }}</td>
-                        <td>{{ $c->mobile }}</td>
-                        <td>{{ $c->email }}</td>
-                        <td>{{ $c->promo_crayola_bunding }}</td>
-                        <td>{{ $c->promo_zip_it }}</td>
-                        <td>{{ $c->promo_none }}</td>
-                        <td>{{ $c->created }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+            <div class="panel panel-default">
+               <div class="panel-heading text-center">
+                   <h2> Wyeth Promil Dashboard </h2>
+               </div>
+                <div class="panel-body">
+                    <table class="table table-hover" id="myTable">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Mobile</th>
+                            <th>Email</th>
+                            <th>Promo Crayola Bunding</th>
+                            <th>Promo Zip It</th>
+                            <th>Promo None</th>
+                            <th>Date Inputted</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($census as $c)
+                            <tr>
+                                <td>{{ $c->name }}</td>
+                                <td>{{ $c->age }}</td>
+                                <td>{{ $c->mobile }}</td>
+                                <td>{{ $c->email }}</td>
+                                <td>{{ $c->parseBooleanCrayola() }}</td>
+                                <td>{{ $c->parseBooleanZipIt() }}</td>
+                                <td>{{ $c->parseBooleanNone() }}</td>
+                                <td>{{ $c->created }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
